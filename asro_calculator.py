5c5d64bf2d7538dad477e05462027e518a5b4763
Chris Woodgate <c.woodgate@warwick.ac.uk>
2025-06-30 11:47:43 +0100

    (Untested) prototype of routine for calculating Warren-Cowley parameters

--- asro_calculator.py
+++ asro_calculator.py
@@ -61,3 +61,52 @@ def get_coordination_numbers(atoms, shell_distances, tol=1e-3):
     total_pairs = total_pairs/len(atoms)
     
     return total_pairs
+
+def asro(atoms, elements, shell_distances, tol=1e-3):
+    '''
+    Return the Warren-Cowley atomic short-range order (ASRO) parameters for a simulation cell described by an Atoms object
+
+            Parameters:
+                    atoms (ASE atoms object): The atoms to consider
+                    elements (list of str): The elements for which to look
+                    shell_distances (list of floats): The distances for which to look
+
+            Returns:
+                    total_pairs (list of floats): List of numbers of atoms on each shell
+    '''
+    
+    cell = atoms.get_cell()
+    
+    positions = atoms.get_positions()
+    
+    labels = atoms.get_chemical_symbols()
+    
+    atoms_distances = atoms.get_all_distances(mic=True, vector=False)
+
+    concentrations = get_concentrations(atoms, elements)
+    
+    coordination_numbers = get_coordination_numbers(atoms, shell_distances)
+    
+    sum_radial_densities = np.zeros((len(shell_distances), len(elements), len(elements)))
+    
+    for i in range(len(atoms)):
+        el1 = elements.index(labels[i])
+        for j in range(len(atoms)):
+            el2 = elements.index(labels[j])
+            for k, dist in enumerate(shell_distances):
+                if np.fabs(dist - atoms_distances[i,j]) < tol:
+                    sum_radial_densities[k, el1, el2] += 1
+    
+    conditional_probabilities = sum_radial_densities/len(atoms)*2
+    
+    for i in range(len(shell_distances)):
+        conditional_probabilities[i] /= coordination_numbers[i]
+    
+    warren_cowleys = np.zeros((len(shell_distances), len(elements), len(elements)))
+    
+    for d in range(len(shell_distances)):
+        for i, ci in enumerate(concentrations):
+            for j, cj in enumerate(concentrations):
+                warren_cowleys[d,i,j] = 1.0 - conditional_probabilities[d,i,j]/cj
+    
+    return warren_cowleys
