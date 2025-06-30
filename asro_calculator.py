46d17369e8a16d067f6e3bab505b0539f1a8d371
Chris Woodgate <c.woodgate@warwick.ac.uk>
2025-06-30 16:00:58 +0100

    Tested version of pair probability and Warren-Cowley functions

--- asro_calculator.py
+++ asro_calculator.py
@@ -4,6 +4,8 @@ Routines for calculation of the Warren-Cowley ASRO parameters for a simulation c
 C. D. Woodgate, University of Bristol, 2025
 '''
 
+import numpy as np
+
 def get_concentrations(atoms, elements):
     '''
     Return the concentrations of elements in a simulation cell described by an atoms object
@@ -20,17 +22,17 @@ def get_concentrations(atoms, elements):
     labels = atoms.get_chemical_symbols()
     
     # Define an array of zeros for calculation of the concentrations
-    concentrations = np.zeros(len(elements))
+    numbers = np.zeros(len(elements))
     
     # Loop over the labels of atoms in the cell
     for label in labels:
         i = elements.index(label)
-        concentrations[i] += 1
+        numbers[i] += 1
     
     # Divide by the number of atoms in the cell
-    concentrations = concentrations/float(len(atoms))
+    concentrations = numbers.copy()/float(len(atoms))
     
-    return concentrations
+    return concentrations, numbers
 
 def get_coordination_numbers(atoms, shell_distances, tol=1e-3):
     '''
@@ -62,9 +64,9 @@ def get_coordination_numbers(atoms, shell_distances, tol=1e-3):
     
     return total_pairs
 
-def asro(atoms, elements, shell_distances, tol=1e-3):
+def asro_conditional_probabilities(atoms, elements, shell_distances, tol=1e-3):
     '''
-    Return the Warren-Cowley atomic short-range order (ASRO) parameters for a simulation cell described by an Atoms object
+    Return the conditional pair probabilities for a simulation cell described by an Atoms object
 
             Parameters:
                     atoms (ASE atoms object): The atoms to consider
@@ -82,8 +84,8 @@ def asro(atoms, elements, shell_distances, tol=1e-3):
     atoms_distances = atoms.get_all_distances(mic=True, vector=False)
 
     # Get concentrations of elements in the cell
-    concentrations = get_concentrations(atoms, elements)
-    
+    concentrations, numbers = get_concentrations(atoms, elements)
+
     # Get coordination numbers of the lattice described by the Atoms object
     coordination_numbers = get_coordination_numbers(atoms, shell_distances, tol)
     
@@ -99,20 +101,42 @@ def asro(atoms, elements, shell_distances, tol=1e-3):
                 if np.fabs(dist - atoms_distances[i,j]) < tol:
                     sum_radial_densities[k, el1, el2] += 1
     
-    # Divide by the number of atoms and multiply by two (want to genuinely count both A-B and B-A pairs!)
-    conditional_probabilities = sum_radial_densities/len(atoms)*2
-    
+    # Empty array for conditional probabilities
+    conditional_probabilities = np.zeros((len(shell_distances), len(elements), len(elements)))
+
     # Convert to conditional probabilities
     for i in range(len(shell_distances)):
-        conditional_probabilities[i] /= coordination_numbers[i]
+        for j in range(len(elements)):
+            conditional_probabilities[i,j,:] = sum_radial_densities[i,j,:]/coordination_numbers[i]/numbers[j]
+
+    return conditional_probabilities
+
+def asro_warren_cowley(atoms, elements, shell_distances, tol=1e-3):
+    '''
+    Return the Warren-Cowley atomic short-range order (ASRO) parameters for a simulation cell described by an Atoms object
+
+            Parameters:
+                    atoms (ASE atoms object): The atoms to consider
+                    elements (list of str): The elements for which to look
+                    shell_distances (list of floats): The distances for which to look
+
+            Returns:
+                    total_pairs (list of floats): List of numbers of atoms on each shell
+    '''
     
+    # Get concentrations of elements in the cell
+    concentrations, numbers = get_concentrations(atoms, elements)
+
+    # Get the conditional probabilities
+    conditional_probabilities = asro_conditional_probabilities(atoms, elements, shell_distances, tol)
+
     # Empty array for Warren-Cowley ASRO parameters
     warren_cowleys = np.zeros((len(shell_distances), len(elements), len(elements)))
-    
+
     # Convert conditional probabilities to Warren-Cowley ASRO parameters
     for d in range(len(shell_distances)):
         for i, ci in enumerate(concentrations):
             for j, cj in enumerate(concentrations):
                 warren_cowleys[d,i,j] = 1.0 - conditional_probabilities[d,i,j]/cj
-    
+
     return warren_cowleys
